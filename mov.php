a2ddbdeef4b2c271adc35216462edcdb20603bd6
Juan Perez <juanperez@gmail.com>
2015-09-19 21:55:51 -0500

    asdas

--- mov.php
+++ mov.php
@@ -10,6 +10,20 @@
 
 http://www.programasprogramacion.com/encriptar.php
 
+
+<div id="ad_300x600">
+	<div class="adconten">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<ins class="adsbygoogle"
+		     style="display:inline-block;width:300px;height:600px"
+		     data-ad-client="ca-pub-5790633931046808"
+		     data-ad-slot="5617309771"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</div>
+</div>
+
 <p>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu cursus nisl. Curabitur pharetra metus quis lectus fringilla, in sodales sem varius. In sodales aliquet urna eget efficitur. Praesent ut diam non magna euismod malesuada sed in nisi. Quisque vitae nunc vel quam gravida aliquet vel et lacus. Nullam tempor, justo ac dignissim pretium, massa metus dapibus lectus, in feugiat nisl sem et purus. Duis suscipit sollicitudin erat ut varius.
 </p>
@@ -293,12 +307,6 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu cursus nisl. Cura
 <p>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu cursus nisl. Curabitur pharetra metus quis lectus fringilla, in sodales sem varius. In sodales aliquet urna eget efficitur. Praesent ut diam non magna euismod malesuada sed in nisi. Quisque vitae nunc vel quam gravida aliquet vel et lacus. Nullam tempor, justo ac dignissim pretium, massa metus dapibus lectus, in feugiat nisl sem et purus. Duis suscipit sollicitudin erat ut varius.
 </p>
-
-
-
-
-
-
 
 
 
@@ -316,6 +324,13 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu cursus nisl. Cura
 
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="./pitagoras.js?r=<?php echo rand(111111,999999); ?>"></script>
+
+
+<!--
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script type="text/javascript" src="https://rawgit.com/adscarlos/slider/master/1804106810.js"></script>
 <script type="text/javascript" src="https://cdn.rawgit.com/adscarlos/slider/master/2190924133.js"></script>
@@ -323,6 +338,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu cursus nisl. Cura
 <script type="text/javascript" src="https://cdn.rawgit.com/adscarlos/slider/master/4158215450.js"></script>
 <script type="text/javascript" src="https://cdn.rawgit.com/adscarlos/slider/master/6551171546.js"></script>
 
+-->
 
 
 </body>
